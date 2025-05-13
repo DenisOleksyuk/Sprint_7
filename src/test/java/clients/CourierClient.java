7ce04172a78db7b5376ee606cc9301457a07c0c1
Denis Oleksyuk <InStanDI@yandex.ru>
2025-05-13 16:46:46 +0300

    добавлена реализация классов. прошу прощения за такой глупость

--- src/test/java/clients/CourierClient.java
+++ src/test/java/clients/CourierClient.java
@@ -1,4 +1,41 @@
 package clients;
 
+import com.google.gson.Gson;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import models.Courier;
+
+import static io.restassured.RestAssured.given;
+
 public class CourierClient {
+    private static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
+    private static final Gson gson = new Gson();
+
+    @Step("Создать курьера")
+    public Response create(Courier courier) {
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .body(gson.toJson(courier)) // Сериализация в JSON
+                .when()
+                .post("/api/v1/courier");
+    }
+
+    @Step("Авторизовать курьера")
+    public Response login(Courier courier) {
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .body(gson.toJson(courier)) // Сериализация в JSON
+                .when()
+                .post("/api/v1/courier/login");
+    }
+
+    @Step("Удалить курьера")
+    public void delete(int courierId) {
+        given()
+                .baseUri(BASE_URL)
+                .when()
+                .delete("/api/v1/courier/" + courierId);
+    }
 }
--- src/test/java/tests/LoginCourierTest.java
+++ src/test/java/tests/LoginCourierTest.java
@@ -1,4 +1,64 @@
 package tests;
 
+import clients.CourierClient;
+import io.qameta.allure.junit4.DisplayName;
+import models.Courier;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+
 public class LoginCourierTest {
+    private CourierClient courierClient;
+    private Courier courier;
+    private int courierId;
+
+    @Before
+    public void setup() {
+        courierClient = new CourierClient();
+        courier = new Courier("test_" + System.currentTimeMillis(), "password", "name");
+        courierClient.create(courier);
+        Response loginResponse = courierClient.login(courier);
+        courierId = loginResponse.then().extract().path("id");
+    }
+
+    @After
+    public void teardown() {
+        courierClient.delete(courierId);
+    }
+
+    @Test
+    @DisplayName("Успешная авторизация курьера")
+    public void testLoginSuccess() {
+        Response response = courierClient.login(courier);
+        response.then()
+                .statusCode(200)
+                .body("id", notNullValue())
+                .body("$", hasKey("id")) // Проверка наличие поля
+                .body("id", is(not(0))); // Проверка что id не нулевой
+    }
+
+    @Test
+    @DisplayName("Авторизация с неверным паролем")
+    public void testLoginWithWrongPassword() {
+        Courier courierWithWrongPassword = new Courier(courier.getLogin(), "wrongPassword", courier.getFirstName());
+        Response response = courierClient.login(courierWithWrongPassword);
+        response.then()
+                .statusCode(404)
+                .body("message", equalTo("Учетная запись не найдена"))
+                .body("$", hasKey("message")); // Проверка структуры месседжа с ошибкой
+    }
+
+    @Test
+    @DisplayName("Авторизация без пароля")
+    public void testLoginWithoutRequiredField() {
+        Courier courierWithoutPassword = new Courier(courier.getLogin(), "", courier.getFirstName());
+        Response response = courierClient.login(courierWithoutPassword);
+        response.then()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для входа"))
+                .body("$", hasKey("message")); // Проверка структуры месседжа с ошибкой
+    }
 }
